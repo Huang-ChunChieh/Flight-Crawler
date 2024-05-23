13cbec6eb107fd3f518f30e081e7c88dce8977b6
Huang-ChunChieh <jay24585978@gmail.com>
2024-05-23 20:10:00 +0800

    add comment

--- kayak.py
+++ kayak.py
@@ -9,12 +9,14 @@ arrive_city = "KIX" #目的地
 depart_time = "2024-07-01" #去程時間 XXXX(西元年)/XX(月)/XX(日)
 arrive_time = "2024-07-04" #返程時間 XXXX(西元年)/XX(月)/XX(日)  若為單程則此欄為""
 cabinclass = "" #艙等 經濟艙:""/特選經濟艙:"premium"/商務艙:"business"/頭等艙:"first"
-numOfAdult = 1
-numOfStudent = 1
-numOfTeenager = 0
-numOfChild = 1
-numOfBaby1S = 0
-numOfBaby1L = 0
+#====================================#
+numOfAdult = 1 #成人數量
+numOfStudent = 1 #18歲以上學生數量
+numOfTeenager = 0 #青少年(12-17歲)數量
+numOfChild = 1 #兒童(2-11)數量
+numOfBaby1S = 0 #2歲以下佔坐兒童數量
+numOfBaby1L = 0 #2歲以下不佔坐兒童數量
+#====================================#
 adult = str(numOfAdult) + "adults" #成人數量 格式:人數填入numOfAdult  若無則此欄為""
 student = str(numOfStudent) + "students" #18歲以上學生數量  格式:人數填入numOfStudent  若無則此欄為""
 children = "children" + "-17"*numOfTeenager + "-11"*numOfChild + "-1S"*numOfBaby1S + "-1L"*numOfBaby1L#18歲以下數量 格式:青少年(12-17):-17 / 兒童(2-11):-11 / 2歲以下佔坐兒童:-1S / 2歲以下不佔坐兒童:-1L / 若無則此欄為""
@@ -47,60 +49,61 @@ else:
     fastFlightSummary_XPATH = '//*[@id="listWrapper"]/div/div[1]/div[3]'
     flightInfo_XPATH = '//*[@id="listWrapper"]/div/div[2]/div/div[2]/div[2]/div/div'
 
+#URL Setting
 url = f"https://www.tw.kayak.com/flights/{depart_city}-{arrive_city}/{depart_time}/{arrive_time}/{cabinclass}/{adult}/{student}/{children}{directflight}"
-url_price = url + "&sort=price_a"
-url_bestflight = url + "&sort=bestflight_a"
-url_duration = url + "&sort=duration_a"
+url_Price = url + "&sort=price_a"
+url_Best = url + "&sort=bestflight_a"
+url_Duration = url + "&sort=duration_a"
 browser = webdriver.Chrome()
 browser.get(url)
 sleep(10)
 
-def get_SUG_flight(): #獲取最便宜/超值/最快三項簡易資訊
+def get_Reco_Flight(): #獲取最便宜/超值/最快三項簡易資訊
     print("推薦航班資訊")
     print("==========================")
-    result_SUG = browser.find_elements(By.CLASS_NAME,"Hv20-option")
-    for z in result_SUG:
+    result_Reco = browser.find_elements(By.CLASS_NAME,"Hv20-option")
+    for z in result_Reco:
         print(z.text)
-    sug_result_message = "推薦航班資訊\n==== ==== ====\n" + "\n".join(a.text for a in result_SUG)
-    return sug_result_message
+    reco_Result_Message = "推薦航班資訊\n==== ==== ====\n" + "\n".join(a.text for a in result_Reco)
+    return reco_Result_Message
 
-def get_cheapest_result():
+def get_Cheapest_Flight():
     print("最優惠航班資訊")
     print("============")
-    cheapest_result_summary = browser.find_element(By.XPATH,cheapestFlightSummary_XPATH) #cheapest_result_box_summary
-    cheapest_result_summary.click() #click cheapest_result_box
+    cheapest_Result_Summary = browser.find_element(By.XPATH,cheapestFlightSummary_XPATH) #cheapest_result_box_summary
+    cheapest_Result_Summary.click() #click cheapest_result_box
     sleep(3)
-    result_cheapest = browser.find_elements(By.XPATH,flightInfo_XPATH) #get cheapest flight info
-    for j in result_cheapest:
+    result_Cheapest = browser.find_elements(By.XPATH,flightInfo_XPATH) #get cheapest flight info
+    for j in result_Cheapest:
         print(j.text)
-    cheapest_result_message = "最優惠航班資訊\n==== ==== ====\n" + j.text + "詳細資訊歡迎點擊下方連結\n" + url_price
-    return cheapest_result_message
+    cheapest_Result_Message = "最優惠航班資訊\n==== ==== ====\n" + j.text + "詳細資訊請點擊下方連結\n" + url_Price
+    return cheapest_Result_Message
 
-def get_best_result():
+def get_Best_Flight():
     print("超值航班資訊")
     print("===========")
-    best_result_summary = browser.find_element(By.XPATH,bestFlightSummary_XPATH) #best_result_box_summary
-    best_result_summary.click() #click best_result_box
+    best_Result_Summary = browser.find_element(By.XPATH,bestFlightSummary_XPATH) #best_result_box_summary
+    best_Result_Summary.click() #click best_result_box
     sleep(3)
-    result_best = browser.find_elements(By.XPATH,flightInfo_XPATH) #get best flight info
-    for i in result_best:
+    result_Best = browser.find_elements(By.XPATH,flightInfo_XPATH) #get best flight info
+    for i in result_Best:
         print(i.text)
-    best_result_message = "超值航班資訊\n==== ==== ====\n" + i.text + "詳細資訊歡迎點擊下方連結\n" + url_bestflight
-    return best_result_message
+    best_Result_Message = "超值航班資訊\n==== ==== ====\n" + i.text + "詳細資訊請點擊下方連結\n" + url_Best
+    return best_Result_Message
 
-
-def get_fast_result():
+def get_Fast_Flight():
     print("最快航班資訊")
     print("===========")
-    fast_result_summary = browser.find_element(By.XPATH,fastFlightSummary_XPATH) #fast_result_box_summary
-    fast_result_summary.click() #click fast_result_box
+    fast_Result_Summary = browser.find_element(By.XPATH,fastFlightSummary_XPATH) #fast_result_box_summary
+    fast_Result_Summary.click() #click fast_result_box
     sleep(3)
-    result_fast = browser.find_elements(By.XPATH,flightInfo_XPATH) #get fast flight info
-    for k in result_fast:
+    result_Fast = browser.find_elements(By.XPATH,flightInfo_XPATH) #get fast flight info
+    for k in result_Fast:
         print(k.text)
-    fast_result_message = "最快航班資訊\n==== ==== ====\n" + k.text + "詳細資訊歡迎點擊下方連結\n" + url_duration
-    return fast_result_message
+    fast_Result_Message = "最快航班資訊\n==== ==== ====\n" + k.text + "詳細資訊請點擊下方連結\n" + url_Duration
+    return fast_Result_Message
 
+#LineNotify
 def lineNotify(token, msg):
     headers = {
         "Authorization" : "Bearer " + token,
@@ -109,22 +112,22 @@ def lineNotify(token, msg):
     payload = {'message' : msg}
     r = requests.post("https://notify-api.line.me/api/notify",headers = headers, params = payload)
     print(r.status_code)
-    return r.status_code
+    return r.status_code #return 200->success
 
 def main():
-    sug_flight_result = get_SUG_flight()
-    cheapest_flight_result = get_cheapest_result()
-    best_flight_result = get_best_result()
-    fast_flight_result = get_fast_result()
-    lineNotify(token, search_Info)
+    reco_Flight_Result = get_Reco_Flight()
+    cheapest_Flight_Result = get_Cheapest_Flight
+    best_Flight_Result = get_Best_Flight()
+    fast_Flight_Result = get_Fast_Flight()
+    lineNotify(token, search_Info) #Send current setting Info
     sleep(2)
-    lineNotify(token, sug_flight_result)
+    lineNotify(token, reco_Flight_Result) #Send Recommendation Info  #TODO 是否保留?
     sleep(2)
-    lineNotify(token, cheapest_flight_result)
+    lineNotify(token, cheapest_Flight_Result) #Send Cheapest Flight Info
     sleep(2)
-    lineNotify(token, best_flight_result)
+    lineNotify(token, best_Flight_Result) #Send Best Flight Info
     sleep(2)
-    lineNotify(token, fast_flight_result)
+    lineNotify(token, fast_Flight_Result) #Send Fast Flight Info
     sleep(2)
 
 token = "XXXXXXXXXXXXXXXXXXXXXXXXXXXX" #Fill in your Token
