12802f32eb4508c5db45c3132a604def14dfb64a
Huang-ChunChieh <jay24585978@gmail.com>
2024-05-26 19:53:59 +0800

    add comment

--- linenotify_airline.py
+++ linenotify_airline.py
@@ -6,19 +6,20 @@ import requests
 #========Info should be setting========#
 token = "XXX" #Fill in your Token
 depart_city = "TPE" #出發地
-arrive_city = "KIX" #目的地
-depart_time = "2024-07-01" #去程時間 XXXX(西元年)/XX(月)/XX(日)
-arrive_time = "2024-07-04" #返程時間 XXXX(西元年)/XX(月)/XX(日)  若為單程則此欄為""
-cabinclass = "" #艙等 經濟艙:""/特選經濟艙:"premium"/商務艙:"business"/頭等艙:"first"
+arrive_city = "KIX" #目的地  若為單程則此欄為""
+depart_time = "2024-07-01" #去程時間 XXXX(西元年)-XX(月)-XX(日)
+arrive_time = "2024-07-04" #返程時間 XXXX(西元年)-XX(月)-XX(日)  若為單程則此欄為""
+cabinclass = "" #艙等  經濟艙:""  特選經濟艙:"premium"  商務艙:"business"  頭等艙:"first"
 directflight = True #是否僅限直飛航班 若是->True  若否->False
-#=========Numbers of Travelers=========#
+#Numbers of Travelers
 numOfAdult = 1 #成人數量
 numOfStudent = 0 #18歲以上學生數量
 numOfTeenager = 0 #青少年(12-17歲)數量
 numOfChild = 0 #兒童(2-11)數量
 numOfBaby1S = 0 #2歲以下佔坐兒童數量
 numOfBaby1L = 0 #2歲以下不佔坐兒童數量
-#==========================================================================#
+#======================================#
+# 旅客人數(轉為網址用)
 if numOfAdult == 0:
     adult = ""
 else:
@@ -31,7 +32,8 @@ if numOfTeenager > 0 or numOfChild > 0 or numOfBaby1S >0 or numOfBaby1L > 0:
     children = "children" + "-17"*numOfTeenager + "-11"*numOfChild + "-1S"*numOfBaby1S + "-1L"*numOfBaby1L
 else:
     children = ""
-#==========================================================================#
+
+# 艙等(轉為顯示查詢條件用)
 if cabinclass == "premium":
     classtype = "特選經濟艙"
 elif cabinclass == "business":
@@ -40,20 +42,22 @@ elif cabinclass == "first":
     classtype = "頭等艙"
 else:
     classtype = "經濟艙"
-#==========================================================================#
+
+# 是否直飛(轉為顯示查詢條件用)
 if directflight == True:
     fdDir = "fs=fdDir=true;stops=~0"
     directFlight = "是"
 else:
     fdDir = ""
     directFlight = "否"
-#==========================================================================#
+
+# 目前查詢條件
 search_Info = f'''\n目前查詢條件\n出發地:{depart_city}\n目的地:{arrive_city}\n出發日期:{depart_time}\n抵達日期:{arrive_time}
 艙等:{classtype}\n僅限直飛航班:{directFlight}\n旅客人數\n成人:{str(numOfAdult)}人\n學生:{str(numOfStudent)}人\n青少年:{str(numOfTeenager)}人\n兒童:{str(numOfChild)}人
 2歲以下佔坐嬰兒:{str(numOfBaby1S)}人\n2歲以下不佔坐嬰兒:{str(numOfBaby1L)}人'''
-#==========================================================================#
+#======================================#
 
-#若人數設定中students非0，則會多顯示需驗證學生身分內容，XPATH需重新定位
+# 人數設定中若Students非0，則會多顯示需驗證學生身分內容，XPATH需重新定位
 if numOfStudent >= 1:
     cheapestFlightSummary_XPATH = '//*[@id="listWrapper"]/div/div[2]/div[1]'
     bestFlightSummary_XPATH = '//*[@id="listWrapper"]/div/div[2]/div[2]'
@@ -95,30 +99,31 @@ else:
 
 #URL Setting
 url = f"https://www.tw.kayak.com/flights/{depart_city}-{arrive_city}/{depart_time}/{arrive_time}/{cabinclass}/{adult}/{student}/{children}?{fdDir}"
-url_Price = url + "&sort=price_a"
-url_Best = url + "&sort=bestflight_a"
-url_Duration = url + "&sort=duration_a"
+url_Price = url + "&sort=price_a" # 價格優先排序
+url_Best = url + "&sort=bestflight_a" # 超值優先排序
+url_Duration = url + "&sort=duration_a" # 最快優先排序
 browser = webdriver.Chrome()
 browser.get(url)
 sleep(10)
 
-def get_Flight_Result():
-    info_GoTripDepartTime = browser.find_element(By.XPATH,GoTripDepartTimeXPATH).text
-    info_GoTripArriveTime = browser.find_element(By.XPATH,GoTripArriveTimeXPATH).text
-    info_GoTripDepartAirport = browser.find_element(By.XPATH,GoTripDepartAirportXPATH).text
-    info_GoTripArriveAirport = browser.find_element(By.XPATH,GoTripArriveAirportXPATH).text
-    info_GoTripDirectFlightOrNot = browser.find_element(By.XPATH,GoTripDirectFlightOrNotXPATH).text
-    info_GoTripTotalTime = browser.find_element(By.XPATH,GoTripTotalTimeXPATH).text
-    info_BackTripDepartTime = browser.find_element(By.XPATH,BackTripDepartTimeXPATH).text
-    info_BackTripArriveTime = browser.find_element(By.XPATH,BackTripArriveTimeXPATH).text
-    info_BackTripDepartAirport = browser.find_element(By.XPATH,BackTripDepartAirportXPATH).text
-    info_BackTripArriveAirport = browser.find_element(By.XPATH,BackTripArriveAirportXPATH).text
-    info_BackTripDirectFlightOrNot = browser.find_element(By.XPATH,BackTripDirectFlightOrNotXPATH).text
-    info_BackTripTotalTime = browser.find_element(By.XPATH,BackTripTotalTimeXPATH).text
-    info_AirlineName = browser.find_element(By.XPATH,AirlineNameXPATH).text
-    info_TotalPricePerPerson = browser.find_element(By.XPATH,TotalPricePerPersonXPATH).text
-    if numOfStudent >= 1:
-        info_StudentOrNot = "**查詢條件中包含學生，需要驗證以獲取學生票價**\n"
+def get_Flight_Result(): # 獲取所需資訊
+    info_GoTripDepartTime = browser.find_element(By.XPATH,GoTripDepartTimeXPATH).text                   # 去程出發時間
+    info_GoTripArriveTime = browser.find_element(By.XPATH,GoTripArriveTimeXPATH).text                   # 去程抵達時間
+    info_GoTripDepartAirport = browser.find_element(By.XPATH,GoTripDepartAirportXPATH).text             # 去程出發機場
+    info_GoTripArriveAirport = browser.find_element(By.XPATH,GoTripArriveAirportXPATH).text             # 去程抵達機場
+    info_GoTripDirectFlightOrNot = browser.find_element(By.XPATH,GoTripDirectFlightOrNotXPATH).text     # 去程航班直飛與否?
+    info_GoTripTotalTime = browser.find_element(By.XPATH,GoTripTotalTimeXPATH).text                     # 去程總時長
+    info_BackTripDepartTime = browser.find_element(By.XPATH,BackTripDepartTimeXPATH).text               # 返程出發時間
+    info_BackTripArriveTime = browser.find_element(By.XPATH,BackTripArriveTimeXPATH).text               # 返程抵達時間
+    info_BackTripDepartAirport = browser.find_element(By.XPATH,BackTripDepartAirportXPATH).text         # 返程出發機場
+    info_BackTripArriveAirport = browser.find_element(By.XPATH,BackTripArriveAirportXPATH).text         # 返程抵達機場
+    info_BackTripDirectFlightOrNot = browser.find_element(By.XPATH,BackTripDirectFlightOrNotXPATH).text # 返程直飛與否?
+    info_BackTripTotalTime = browser.find_element(By.XPATH,BackTripTotalTimeXPATH).text                 # 返程總時長
+    info_AirlineName = browser.find_element(By.XPATH,AirlineNameXPATH).text                             # 航空公司名
+    info_TotalPricePerPerson = browser.find_element(By.XPATH,TotalPricePerPersonXPATH).text             # 總價格(/人)
+
+    if numOfStudent >= 1:   # 查詢條件中包含學生，需要驗證以獲取學生票價 => 回傳已通知使用者
+        info_StudentOrNot = "**查詢條件中包含學生，需要驗證以獲取學生票價**\n" 
     else:
         info_StudentOrNot = ""
     flightResult = f'''{info_StudentOrNot}去程\n時間:{info_GoTripDepartTime}-{info_GoTripArriveTime}\n出發機場:{info_GoTripDepartAirport}\n目的地機場:{info_GoTripArriveAirport}\n直飛航班or轉機:{info_GoTripDirectFlightOrNot}\n總飛行時間:{info_GoTripTotalTime}
@@ -127,7 +132,7 @@ def get_Flight_Result():
     print(flightResult)
     return flightResult
 
-def get_Reco_Flight(): #獲取最便宜/超值/最快三項簡易資訊
+def get_Reco_Flight(): # 獲取最便宜/超值/最快三項簡易資訊
     print("推薦航班資訊")
     print("==========================")
     result_Reco = browser.find_elements(By.CLASS_NAME,"Hv20-option")
@@ -136,7 +141,7 @@ def get_Reco_Flight(): #獲取最便宜/超值/最快三項簡易資訊
     reco_Result_Message = "\n推薦航班資訊\n====================\n" + "\n".join(a.text for a in result_Reco)
     return reco_Result_Message
 
-def get_Cheapest_Flight():
+def get_Cheapest_Flight():  # 獲取最便宜航空資訊
     print("最優惠航班資訊")
     print("============")
     cheapest_Result_Summary = browser.find_element(By.XPATH,cheapestFlightSummary_XPATH) #cheapest_result_box_summary
@@ -148,7 +153,7 @@ def get_Cheapest_Flight():
     cheapest_Result_Message = "\n最優惠航班資訊\n====================\n" + get_Flight_Result() + "詳細資訊請點擊下方連結\n" + url_Price
     return cheapest_Result_Message
 
-def get_Best_Flight():
+def get_Best_Flight():  # 獲取最佳(超值)航空資訊
     print("超值航班資訊")
     print("===========")
     best_Result_Summary = browser.find_element(By.XPATH,bestFlightSummary_XPATH) #best_result_box_summary
@@ -160,7 +165,7 @@ def get_Best_Flight():
     best_Result_Message = "\n超值航班資訊\n====================\n" + get_Flight_Result() + "詳細資訊請點擊下方連結\n" + url_Best
     return best_Result_Message
 
-def get_Fast_Flight():
+def get_Fast_Flight(): # 獲取最快航空資訊
     print("最快航班資訊")
     print("===========")
     fast_Result_Summary = browser.find_element(By.XPATH,fastFlightSummary_XPATH) #fast_result_box_summary
